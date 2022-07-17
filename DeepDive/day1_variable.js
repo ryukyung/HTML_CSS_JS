648b6bf38e611366a5e7d816ba2351891e5f3436
ryukyunghye <ryukyung123@naver.com>
2022-07-17 21:08:11 +0900

    Edit(repo)

---
+++ DeepDive/day1_variable.js
@@ -0,0 +1,13 @@
+// Q1. 변수 가지고 놀기
+let admin;
+let name;
+name = "John";
+admin = name;
+window.alert(admin);
+// Q2. 올바른 이름 선택하기
+let planeName = "Earth";
+let userName = "John";
+
+// Q3. 대문자 상수 올바로 사용하기
+const BIRTHDAY = "18.04.1982";
+const AGE = someCode(BIRTHDAY);

7f9d8b07b130552d85709dd47c89799fae96523d
ryukyunghye <ryukyung123@naver.com>
2022-07-20 01:13:18 +0900

    Add(file): deepdive day2

---
+++ DeepDive/day2_dataType.js
@@ -0,0 +1,9 @@
+// Q1. 문자열 따옴표
+let name = "Ilya";
+
+alert( `hello ${1}` ); // hello 1 : number
+
+alert( `hello ${"name"}` ); // hello name : answkduf
+
+alert( `hello ${name}` ); // hello Ilya
+// 표현식에 name라는 변수가 있다 이 name이라는 변수가 평가되어야 전체 문자열을 반환할 수 있다
